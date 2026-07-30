7b8b8ac238548b39297423a659341d9aa7f3a642
ajaykrixna <ajaykrishnavk04@gmail.com>
2026-07-30 12:05:21 +0530

    Add Todo model and ignore generated files

---
+++ todo-backend/app/models/todo.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Boolean
+
+from app.database import Base
+
+
+
+class Todo(Base):
+
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    title = Column(String)
+
+    description = Column(String)
+
+    completed = Column(Boolean, default=False)
