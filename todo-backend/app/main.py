516dc3da2c762db7efc1b1a43215e4a8cba2bf8f
ajaykrixna <ajaykrishnavk04@gmail.com>
2026-07-31 16:47:59 +0530

    Updated main

--- todo-backend/app/main.py
+++ todo-backend/app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
-from app.database import Base, engine
-from app.models.todo import Todo
 
+from app.database import Base, engine
+from app.models import todo
+from app.routers.todo import router
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.include_router(router)
+
 
 @app.get("/")
 def root():
     return {"message": "Todo Backend API is running!"}
-
-Base.metadata.create_all(bind=engine)
